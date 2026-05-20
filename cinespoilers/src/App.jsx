ef5f3c266cf449073a4a48954750b7354b30ef98
zHyoh <mpl1297@hotmail.com>
2026-05-19 21:30:06 -0500

    feat: create card component

--- cinespoilers/src/App.jsx
+++ cinespoilers/src/App.jsx
@@ -1,23 +1,11 @@
-const Card = () => {
-  return (
-    <div>
-    <header>
-      Card Title
-    </header>
-    <div>
-    Card Body
-    </div>
-    <footer>
-    Card footer
-    </footer>
-    </div>
-  );
-};
+import Card from "./components/Card"
 
 function App() {
   return (
     <>
       <h1>CineSpoilers</h1>
+      <Card/>
+      <Card/>
       <Card/>
     </>
   )
---
+++ cinespoilers/src/components/Card.jsx
@@ -0,0 +1,17 @@
+const Card = () => {
+    return (
+        <div className="card">
+            <header>
+                Card Title
+            </header>
+            <div>
+                Card Body
+            </div>
+            <footer>
+                Card footer
+            </footer>
+        </div>
+    );
+};
+
+export default Card
